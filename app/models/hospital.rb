4257935c8503c0be01368265d5a59fd10426517f
Nick King <nickmaxking@gmail.com>
2020-11-09 10:14:52 -0700

    Add doctor_count and unique_universities instance methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctor_count
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.select(:university).distinct.pluck(:university)
+  end
 end
