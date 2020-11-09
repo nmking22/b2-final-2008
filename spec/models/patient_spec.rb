3319b9839ded80065b43a8a377b072482a268c98
Nick King <nickmaxking@gmail.com>
2020-11-09 10:42:34 -0700

    Add test for find_doctor_patients instance method

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,58 @@ describe Patient, type: :model do
     it { should have_many :doctor_patients }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  describe 'instance_methods' do
+    it '#find_doctor_patient' do
+      grey_sloan = Hospital.create!(
+        name: "Grey Sloan Memorial Hospital"
+      )
+      dr_grey = Doctor.create!(
+        name: "Meredith Grey",
+        specialty: "General Surgery",
+        university: "Harvard University",
+        hospital: grey_sloan
+      )
+      dr_karev = Doctor.create!(
+        name: "Alex Karev",
+        specialty: "Pediatric Surgery",
+        university: "Harvard University",
+        hospital: grey_sloan
+      )
+      dr_bailey = Doctor.create!(
+        name: "Miranda Bailey",
+        specialty: "General Surgery",
+        university: "Stanford University",
+        hospital: grey_sloan
+      )
+      dr_mcdreamy = Doctor.create!(
+        name: "Derek McDreamy",
+        specialty: "Attending Surgeon",
+        university: "University of Pennsylvania",
+        hospital: grey_sloan
+      )
+      patient_1 = Patient.create!(
+        name: "Katie Bryce",
+        age: 24
+      )
+      doctor_patient_1 = DoctorPatient.create!(
+        doctor: dr_grey,
+        patient: patient_1
+      )
+      doctor_patient_2 = DoctorPatient.create!(
+        doctor: dr_karev,
+        patient: patient_1
+      )
+      doctor_patient_3 = DoctorPatient.create!(
+        doctor: dr_bailey,
+        patient: patient_1
+      )
+      doctor_patient_4 = DoctorPatient.create!(
+        doctor: dr_mcdreamy,
+        patient: patient_1
+      )
+
+      expect(patient_1.find_doctor_patient(dr_grey)).to eq(doctor_patient_1)
+    end
+  end
 end
