b27c846e6a332ced44f79e9492113d9d7d3d104a
Nick King <nickmaxking@gmail.com>
2020-11-09 11:30:36 -0700

    Add oldest_to_youngest class method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,4 +5,8 @@ class Patient < ApplicationRecord
   def find_doctor_patient(doctor)
     doctor_patients.where(doctor: doctor).first
   end
+
+  def self.oldest_to_youngest
+    Patient.order(age: :desc)
+  end
 end
